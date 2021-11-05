e0be3dfa77a1a8246216fbdb707840e815f1ffd8
dandlp95 <del17011@byui.edu>
2021-11-04 22:57:24 -0600

    table

--- lesson 7/scripts/app.js
+++ lesson 7/scripts/app.js
@@ -4,27 +4,31 @@ const loadImages = (image) => {
   image.onload = () => {
     image.removeAttribute('data-src');
   };
+}
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px -50px 0px'
 };
 
-imagesToLoad.forEach((img) => {
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
     loadImages(img);
   });
+}
 
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
-   
+
+
+
