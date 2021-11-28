c34276192892a0c15e71bffbe1d6d05ecb54b0ac
dandlp95 <del17011@byui.edu>
2021-11-27 22:48:51 -0700

    done

--- lesson11/Home/script/home.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-    document.getElementById("mainNav").classList.toggle('hide');
-}
-
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var date = new Date();
-var n = date.toLocaleDateString('en-UK', options);
-document.getElementById("todayDate").innerHTML = n;
