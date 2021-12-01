b291d0a7fa5a13478ae3ec23c3d8ca9dcf989b28
dandlp95 <del17011@byui.edu>
2021-11-30 23:44:51 -0700

    lesson07

---
+++ lesson07/scripts/scripts.js
@@ -0,0 +1,49 @@
+function toggleMenu(){
+    document.getElementById("mainNav").classList.toggle('hide');
+}
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var date = new Date();
+var n = date.toLocaleDateString('en-UK', options);
+document.getElementById("todayDate").innerHTML = n;
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+
+
+
+let daysPassed = document.getElementById("daysPassed").textContent;
+let pageVisit = localStorage.getItem('daysPassed') || date;
+let days = Math.round((pageVisit - date)/8640000);
+
+daysPassed.textContent = days;
+
+localStorage.setItem("daysPassed", daysPassed)
